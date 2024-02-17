872b0f702c7062b9de10c7a7482729a521dc9524
zulu <a@b.com>
2024-02-16 22:04:09 -0500

    Fix incorrect merge

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
-import App from "./App"
+import TemperatureBarStack from "./TopicRepoStack"
 import reportWebVitals from "./reportWebVitals"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(
-<<<<<<< Updated upstream
   <React.StrictMode>
-    <BrowserRouter>
     <TemperatureBarStack/>
-    </BrowserRouter>
   </React.StrictMode>
 );
-=======
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
-)
->>>>>>> Stashed changes
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
